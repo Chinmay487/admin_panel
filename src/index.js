6933fb1d7e4348a792e7073ac5c6fa5389674e5d
Chinmay487 <pchinmay04@gmail.com>
2022-01-18 13:03:16 +0530

    routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import  App from './App';
+import {BrowserRouter} from '@mui/material';
 
 ReactDOM.render(
-
-  <>
+  <BrowserRouter>
     <App/>
-  </>,
+  </BrowserRouter>,
   document.getElementById('root')
 )
